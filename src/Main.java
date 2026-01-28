12fef0a0c737b10d1a477817ecc632fef670890a
Diana BUYINZA <buyinzadiana@gmail.com>
2026-01-28 10:25:28 +0200

    try and catch example 2 multiple catch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        try {
+//            String text = null;
+//            System.out.println(text.length());
+//
+//        } catch (NullPointerException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-        try {
-            String text = null;
-            System.out.println(text.length());
-
-        } catch (NullPointerException e) {
-            e.printStackTrace();
+    public static void main(String[] args){
+        try
+        {
+        String text = "lorem ipsum";
+        int textLength = text.length();
+        int dividing = textLength / 2;
+        System.out.println(dividing);
+    }
+    catch (NullPointerException e){
+        System.out.println("text was null");
+    }
+        catch (ArithmeticException e){
+            System.out.println("cant divide by zero");
         }
-    }}
+}
+}
